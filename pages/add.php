58f48bc1e08e04feb5c678dc948a7dbc25da8417
Xyrus <jxyrus010@gmail.com>
2026-09-14 07:24:04 +0800

    need to revert some progress

--- pages/add.php
+++ pages/add.php
@@ -10,14 +10,14 @@
         <div id="header_margin">
             <a href="../homepage.html" id="icon_container">
                 <div id="icon">
-                    
+                    <p style="color: #191919; font-size:24px">=</p>
                 </div>
-                <p style="color: #E05153;">Homepage</p>
+                <p style="color: #DBDBDB;">Homepage</p>
             </a>
 
             <div id="selection">
                 <a href="add.php">
-                    <p>Add</p>
+                    <p style="color: #E05153;">Add</p>
                 </a>
                 <a href="sub.php">
                     <p>Subtract</p>
@@ -32,33 +32,67 @@
         </div>
     </div>
 
+<!------------ section: FORM -------------->
     <div id="form">
-        <div id="form_section">
-            <div class="container">
-                <div id="headings">
-                    <h2>Addition Calculator</h2>
-                    <p text-align: justify;>Combine two or more numbers into a sum</p>
-                </div>
-
-<!------------ section: PHP PROCESS -------------->
-                <div id="input_area">
-                    <form action="" method="POST">
-                        <input class="numfield" type="text" name="num1" placeholder="First Number">
-                        <input class="numfield" type="text" name="num2" placeholder="Second Number">
-                        <input id="compute" type="submit" value="Compute Sum">
-                    </form>
-                </div>
-
-                <div id="sum">
-                    <p>Result:</p>
-                    <div>
-                        
+        <div id="form_container">
+            <div id="form_section">
+                <div class="container">
+                    <div id="headings">
+                        <h2>Addition Calculator</h2>
+                        <p text-align: justify;>Combine two or more numbers into a sum</p>
                     </div>
-                </div>
-            </div>  
+
+    <!------------ section: PHP PROCESS -------------->
+                    <div id="input_area">
+                        <form action="" method="POST">
+                            <input class="numfield" type="text" name="num1" placeholder="First Number">
+                            <input class="numfield" type="text" name="num2" placeholder="Second Number">
+                            <input id="compute" type="submit" name="compute" value="Compute Sum">
+                        </form>
+                    </div>
+    <!-- CODE HERE -->
+                    <?php
+                    $sum = 0;
+
+                    if (isset($_POST['compute'])){
+                        $num1 = $_POST['num1'];
+                        $num2 = $_POST['num2'];
+                        $sum = $num1 + $num2;
+                    }
+                    
+                    ?>
+
+                    <div id="result">
+                        <p>Result:</p>
+                        <div>
+                            <p>
+                                <?php
+                                    echo $sum
+                                ?>
+                            </p>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+
+            <div id="wallpaper">
+                <img src="../images/galaxy.png" alt="">
+            </div>
         </div>
-        <div id="wallpaper">
-            <img src="../images/galaxy.png" alt="">
+        </div>
+            <div id="other_op">
+                <p>Other Operators</p>
+                <div id="ops">
+                    <a href="sub.php" class="operator_container">
+                        <img src="../images/operators/subtract_white.svg">
+                    </a>
+                    <a href="mul.php" class="operator_container">
+                        <img src="../images/operators/multiply_white.svg">
+                    </a>
+                    <a href="div.php" class="operator_container">
+                        <img src="../images/operators/divide_white.svg">
+                    </a>
+                </div>
         </div>
     </div>  
 </body>
